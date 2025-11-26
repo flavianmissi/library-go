bdb20a8740802c5657ec6535899c051ed660b926
Flavian Missi <fmissi@redhat.com>
2025-11-26 17:14:03 +0100

    drop unix:// from plugin socket path

--- pkg/operator/encryption/kms/kms.go
+++ pkg/operator/encryption/kms/kms.go
@@ -11,7 +11,7 @@ import (
 
 const (
 	// unixSocketBaseDir is the base directory for KMS unix sockets
-	unixSocketBaseDir = "unix:///var/run/kms"
+	unixSocketBaseDir = "/var/run/kms"
 )
 
 // GenerateUnixSocketPath generates a unique unix socket path from KMS configuration
